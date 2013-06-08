e46702c172908140659c92c001a3fcaee1f78592
Fomin Arseny <arseniy-fe@mail.ru>
2013-06-08 14:10:03 +0700

    add main file - test

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "MainWindow.h"
+#include <QtGui/QApplication>
+
+//#include "Vector.h"
+
+int main(int argc, char *argv[])
+{
+	//{
+	//	Vector<int> v1;
+	//	Vector<int> v2(5);
+	//	Vector<int> v3(5, 0);
+	//	Vector<int> v4(v1);
+	//	Vector<int> v5 = v1;
+	//	Vector<int> v6(7, 32);
+	//	v6 = v2;
+	//}
+
+	QApplication a(argc, argv);
+	SensorNet w;
+	w.show();
+	return a.exec();
+}
